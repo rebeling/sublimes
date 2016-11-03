c2860d13e9395fc1bf4a0cdab5cc358d9fddf361
Matthias Rebel <webmaster@rebeling.net>
2016-11-03 21:41:16 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# according to the specs ;)
+# packages: GitGutter, PackageControl, SublimeLinter, SublimeLinter-pep8,
+#           SublimeREPL, Theme Soda, PackageResourcesViewer, 
+#           SidebarEnhancements, SublimeCodIntel
+# to change sidebar style use packageresource theme
+ln -s ~/sublimes/sublime-settings-User.json ~/Library/Application Support/Sublime Text 3/Packages/Default/Preferences.sublime-settings
+ln -s ~/sublimes/Snippets ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Python/Snippets
+
