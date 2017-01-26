430cd312ee0e98e8a4ca40ceb8a0f3cec3ed3099
Matthias Rebel <webmaster@rebeling.net>
2017-01-26 23:19:22 +0100

    Update settings

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # according to the specs ;)
-# packages: GitGutter, PackageControl, SublimeLinter, SublimeLinter-pep8,
+# packages: GitGutter, SublimeLinter, SublimeLinter-pep8,
 #           SublimeREPL, Theme Soda, PackageResourcesViewer, 
 #           SidebarEnhancements, SublimeCodIntel
 # to change sidebar style use packageresource theme
-ln -s ~/sublimes/sublime-settings-User.json ~/Library/Application Support/Sublime Text 3/Packages/Default/Preferences.sublime-settings
-ln -s ~/sublimes/Snippets ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Python/Snippets
+ln -s ~/sublimes/sublime-settings-User.json ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 
+ln -s ~/sublimes/Snippets ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Snippets
+
+ln -s ~/sublimes/package_control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
+
+# ln -s ~/sublimes/Soda\ Dark\ 3.sublime-theme ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Soda\ Dark\ 3.sublime-theme
+
+ln -s ~/sublimes/Default.sublime-theme ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default.sublime-theme
